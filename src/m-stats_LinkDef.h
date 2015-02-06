a5683c32fd0278f23d9f087c5572175ec34e4453
Matteo Agostini <matteo.agostini@ph.tum.de>
2015-02-06 17:13:25 +0100

    Added typedefs to LinkDef

--- src/m-stats_LinkDef.h
+++ src/m-stats_LinkDef.h
@@ -5,12 +5,19 @@
 
 // MSConfig.h
 #pragma link C++ class mst::MSConfig-;
+#pragma link C++ typedef mst::MSConfigSubSetPair;
+#pragma link C++ typedef mst::MSConfigSubSetMap;
+#pragma link C++ typedef mst::MSConfigPair;
+#pragma link C++ typedef mst::MSConfigMap;
 
 // MSDataObject.h:
 #pragma link C++ class mst::MSDataObject-;
 
 // MSDataPoint.h:
 #pragma link C++ class mst::MSDataPoint-;
+#pragma link C++ typedef mst::MSDataPointPair;
+#pragma link C++ typedef mst::MSDataPointMap;
+#pragma link C++ typedef mst::MSDataPointVector;
 
 // MSDataSet.h
 #pragma link C++ class mst::MSDataSet-;
@@ -20,9 +27,13 @@
 
 // MSModel.h
 #pragma link C++ class mst::MSModel-;
+#pragma link C++ typedef mst::MSModelVector
 
 // MSParameter.h
 #pragma link C++ class mst::MSParameter-;
+// #pragma link C++ typedef mst::MSParameterVector
+#pragma link C++ typedef mst::MSParameterMap
+#pragma link C++ typedef mst::MSParameterPair
 
 #endif // __CINT__
 
