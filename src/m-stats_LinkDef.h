0ed322d29554e20a8958f485046116b2a3ddcad6
Matteo Agostini <matteo.agostini@ph.tum.de>
2015-02-11 09:39:54 +0100

    removed typedef's whose visibility is limited to a class

--- src/m-stats_LinkDef.h
+++ src/m-stats_LinkDef.h
@@ -4,36 +4,36 @@
 #ifdef __CINT__
 
 // MSConfig.h
-#pragma link C++ class mst::MSConfig-;
-#pragma link C++ typedef mst::MSConfigSubSetPair;
-#pragma link C++ typedef mst::MSConfigSubSetMap;
-#pragma link C++ typedef mst::MSConfigPair;
-#pragma link C++ typedef mst::MSConfigMap;
+#pragma link C++ class   mst::MSConfig-;
+//#pragma link C++ typedef mst::MSConfigSubSetPair;
+//#pragma link C++ typedef mst::MSConfigSubSetMap;
+//#pragma link C++ typedef mst::MSConfigPair;
+//#pragma link C++ typedef mst::MSConfigMap;
 
 // MSDataObject.h:
-#pragma link C++ class mst::MSDataObject-;
+#pragma link C++ class   mst::MSDataObject;
 
 // MSDataPoint.h:
-#pragma link C++ class mst::MSDataPoint-;
-#pragma link C++ typedef mst::MSDataPointPair;
-#pragma link C++ typedef mst::MSDataPointMap;
+#pragma link C++ class   mst::MSDataPoint-;
+//#pragma link C++ typedef mst::MSDataPointPair;
+//#pragma link C++ typedef mst::MSDataPointMap;
 #pragma link C++ typedef mst::MSDataPointVector;
 
 // MSDataSet.h
-#pragma link C++ class mst::MSDataSet-;
+#pragma link C++ class   mst::MSDataSet-;
 
 // MSMinimizer.h
-#pragma link C++ class mst::MSMinimizer-;
+#pragma link C++ class   mst::MSMinimizer-;
 
 // MSModel.h
-#pragma link C++ class mst::MSModel-;
-#pragma link C++ typedef mst::MSModelVector
+#pragma link C++ class   mst::MSModel-;
+#pragma link C++ typedef mst::MSModelVector;
 
 // MSParameter.h
-#pragma link C++ class mst::MSParameter-;
+#pragma link C++ class   mst::MSParameter-;
 // #pragma link C++ typedef mst::MSParameterVector
-#pragma link C++ typedef mst::MSParameterMap
-#pragma link C++ typedef mst::MSParameterPair
+#pragma link C++ typedef mst::MSParameterMap;
+#pragma link C++ typedef mst::MSParameterPair;
 
 #endif // __CINT__
 
