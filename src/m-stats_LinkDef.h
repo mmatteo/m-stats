125495c2fda3a03cea5ccc5b98444ecaba688113
Matteo Agostini <matteo.agostini@ph.tum.de>
2018-01-03 12:30:51 +0100

    moved linkdef to cling

--- src/m-stats_LinkDef.h
+++ src/m-stats_LinkDef.h
@@ -1,20 +1,20 @@
 #ifndef m_stats_LinkDef_h
 #define m_stats_LinkDef_h
 
-#ifdef __CINT__
+#ifdef __CLING__
 
-#pragma link C++ class mst::MSObject;
-#pragma link C++ class mst::MSMath-;
-#pragma link C++ class mst::MSConfig-;
-#pragma link C++ class mst::MSDataPoint-;
-#pragma link C++ class mst::MSDataPointVector-;
-#pragma link C++ class mst::MSPDFBuilderTHn-;
-#pragma link C++ class mst::MSParameter-;
-#pragma link C++ class mst::MSModel-;
-#pragma link C++ class mst::MSModelTHnBMLF-;
-#pragma link C++ class mst::MSModelPullGaus-;
-#pragma link C++ class mst::MSMinimizer-;
+#pragma link C++ class mst::MSObject-!;
+#pragma link C++ class mst::MSMath-!;
+#pragma link C++ class mst::MSConfig-!;
+#pragma link C++ class mst::MSDataPoint-!;
+#pragma link C++ class mst::MSDataPointVector-!;
+#pragma link C++ class mst::MSPDFBuilderTHn-!;
+#pragma link C++ class mst::MSParameter-!;
+#pragma link C++ class mst::MSModel-!;
+#pragma link C++ class mst::MSModelTHnBMLF-!;
+#pragma link C++ class mst::MSModelPullGaus-!;
+#pragma link C++ class mst::MSMinimizer-!;
 
-#endif // __CINT__
+#endif // __CLING__
 
 #endif // m_stats_LinkDef_h
